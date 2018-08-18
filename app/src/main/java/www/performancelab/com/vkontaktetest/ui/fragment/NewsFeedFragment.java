986a29454861af169e963f5e8f02e77cc42c2107
kopysovdenis <kopysovdenisalexeevich@gmail.com>
2018-08-18 14:18:26 +0300

    Fix all bug

--- app/src/main/java/www/performancelab/com/vkontaktetest/ui/fragment/NewsFeedFragment.java
+++ app/src/main/java/www/performancelab/com/vkontaktetest/ui/fragment/NewsFeedFragment.java
@@ -11,6 +11,7 @@ import com.arellomobile.mvp.presenter.InjectPresenter;
 import javax.inject.Inject;
 
 import www.performancelab.com.vkontaktetest.MyApplication;
+import www.performancelab.com.vkontaktetest.R;
 import www.performancelab.com.vkontaktetest.mvp.presenter.BaseFeedPresenter;
 import www.performancelab.com.vkontaktetest.mvp.presenter.NewsFeedPresenter;
 import www.performancelab.com.vkontaktetest.rest.api.WallApi;
@@ -36,12 +37,15 @@ public class NewsFeedFragment extends BaseFeedFragment{
         MyApplication.getsApplicationComponent().inject(this);
     }
 
-    @SuppressLint("CheckResult")
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
     }
 
+    @Override
+    public int onCreateToolBarTitle() {
+        return R.string.screen_name_news;
+    }
     @Override
     protected BaseFeedPresenter onCreateFeedPresenter() {
         return mPresenter;
